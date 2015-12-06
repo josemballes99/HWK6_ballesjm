3753f096183f9c4b11971ef4eda6477644be28a1
Rehan Theiveehathasan <rehant@users.noreply.github.com>
2015-12-06 05:16:55 -0500

    Made it refer to .h files not .hpp

--- ArithmeticExpression.h
+++ ArithmeticExpression.h
@@ -1,5 +1,5 @@
 //
-//  ArithmeticExpression.hpp
+//  ArithmeticExpression.h
 //  HWK6_ballesjm
 //
 //  Created by Jose Miguel Ballesteros on 2015-12-02.
@@ -23,13 +23,13 @@ using namespace std;
 //class ArithmeticExpression : public Expression {
 //    Expression *left;
 //    Expression *right;
-//    
+//
 //    string evaluate (){ //evaluate left expression and right expression
 //        void print();
 //    }
-//    
+//
 //    float convert (string s){ // Converts a string (as would be returned by evaluate) to a float
-//        
+//
 //    }
 //};
 
